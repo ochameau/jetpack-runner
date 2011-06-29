d47281c515d379b886962bc61d3fc0381348c62e
Alexandre Poirot <poirot.alex@gmail.com>
2011-06-29 19:17:38 +0200

    Fix package.json's main attribute handling (support relative path, consider it absolute to the package folder). Improve whole toolchain tests.

--- tests/test-whole-toolchain.js
+++ tests/test-whole-toolchain.js
@@ -5,7 +5,14 @@ const AddonOptions = require("addon-options");
 const path = require("path");
 const fs = require("fs");
 const XpiBuilder = require("xpi-builder");
-const AddpnRunner = require("addon-runner");
+const AddonRunner = require("addon-runner");
+const { Ci, Cc } = require("chrome");
+
+// Disable cache in order to speedup this test
+const prefManager = Cc["@mozilla.org/preferences-service;1"]
+                    .getService(Ci.nsIPrefBranch);
+prefManager.setBoolPref("extensions.getAddons.cache.enabled", false);
+
 
 function getPackages(file) {
   let url = self.data.url("tests/toolchain/"+file);
@@ -26,10 +33,9 @@ function getXpiPath() {
 }
 
 function runWithinAndCheck(test, xpiPath, jetpackID) {
-  test.waitUntilDone(30000);
   let success = false;
   
-  let p = AddpnRunner.runWithin({
+  let p = AddonRunner.runWithin({
     binary: require("moz-bin-search").getBestBinary(),
     xpiPath: xpiPath,
     jetpackID: jetpackID,
@@ -43,7 +49,6 @@ function runWithinAndCheck(test, xpiPath, jetpackID) {
         console.log("Got data : >>"+data+"<<");
     },
     quit: function (data) {
-      console.log(">>>>>>>>>>>>>>>>>>>> QUIT WITHIN");
       if (path.existsSync(xpiPath))
         fs.unlinkSync(xpiPath);
       if (!success)
@@ -57,248 +62,294 @@ function runWithinAndCheck(test, xpiPath, jetpackID) {
   });
 }
 
-exports.testManifestLibFirstPriority = function (test) {
-  var prefManager = Components.classes["@mozilla.org/preferences-service;1"]
-                                .getService(Components.interfaces.nsIPrefBranch);
-  prefManager.setBoolPref("extensions.getAddons.cache.enabled", false);
+function runTest(test, input) {
+  test.waitUntilDone(30000);
   
+  // Read packages folders and package.json files
+  let packages = getPackages(input.packagesPath);
   
-  let name = "lib-first-priority";
-  let packages = getPackages("folder-priorities/" + name);
-  test.assertEqual(packages[name].lib.length, 1, "We got only one module folder registered");
-  test.assertEqual(packages[name].lib[0], "manifest-lib", "`lib` attribute in package.json is the first priority for specifying modules folder");
+  // Checkpoint: verify packages-inspector results
+  if (typeof input.checkPackages == "function")
+    input.checkPackages(packages);
   
+  // Load api-utils package
   packages = includeApiUtils(packages);
   
+  // Build manifest
   let options = AddonOptions.buildForRun({
       packages: packages,
-      mainPackageName : name,
+      mainPackageName : input.mainPackageName,
     });
   
-  test.assert(name in options.metadata, "options.metadata is correct");
+  // Checkpoint: verify addon-options result
+  if (typeof input.checkManifest == "function")
+    input.checkManifest(options);
   
+  // Build XPI file
   let xpiPath = getXpiPath();
+  let newOptions = XpiBuilder.build(options, input.mainPackageName, xpiPath);
   
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
+  // Run it in same Firefox instance
+  // and ensure that we get "Test OK" from STDOUT
   runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+}
+
+function assertRequire(test, options, packageName, section, module, requires, message) {
+  let url = options.uriPrefix + packageName + "-" + section + "/" + module;
+  let manifest = options.manifest[url];
+  test.assertEqual(Object.keys(manifest.requirements).sort().join(", "),
+                   requires.sort().join(", "),
+                   message);
+}
+
+exports.testManifestLibFirstPriority = function (test) {
+  let name = "lib-first-priority";
+  runTest(test, {
+    packagesPath: "folder-priorities/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].lib.length, 1, "We got only one module folder registered");
+      test.assertEqual(packages[name].lib[0], "manifest-lib", "`lib` attribute in package.json is the first priority for specifying modules folder");
+    },
+    checkManifest: function (options) {
+      test.assert(name in options.metadata, "options.metadata is correct");
+    }
+  });
 }
 
 exports.testManifestDirectoriesSecondPriority = function (test) {
   let name = "directories-second-priority";
-  let packages = getPackages("folder-priorities/" + name);
-  test.assertEqual(packages[name].lib[0], "directories-lib", "When no `lib` attribute is specified, `directories.lib` attribute become the next priority for specifying modules folder");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "folder-priorities/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].lib[0], "directories-lib", "When no `lib` attribute is specified, `directories.lib` attribute become the next priority for specifying modules folder");
+    },
+    checkManifest: function (options) {
+      
+    }
+  });
 }
 
 exports.testRootLastPriority = function (test) {
   let name = "root-last-priority";
-  let packages = getPackages("folder-priorities/" + name);
-  test.assertEqual(packages[name].lib[0], ".", "When no `lib`, nor `directories.lib` attributes are specified, root folder is used as modules folder");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "folder-priorities/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].lib[0], ".", "When no `lib`, nor `directories.lib` attributes are specified, root folder is used as modules folder");
+    },
+    checkManifest: function (options) {
+      
+    }
+  });
 }
 
 exports.testManifestWithoutMain = function (test) {
   let name = "main-default";
-  let packages = getPackages("main/" + name);
-  test.assertEqual(packages[name].main, "main", "When `main` attributes is ommitted and main.js exists, this module is used as the main entry point.");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "main/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].main, "main", "When `main` attributes is ommitted and main.js exists, this module is used as the main entry point.");
+    },
+    checkManifest: function (options) {
+      test.assertEqual(options.main, name + "/main",
+        "Manifest `main` is correct, i.e. absolute path to main module.");
+    }
+  });
 }
 
 exports.testManifestWithMain = function (test) {
   let name = "main-specified";
-  let packages = getPackages("main/" + name);
-  test.assertEqual(packages[name].main, "specified", "When `main` attributes is set, the specified module is used as the main entry point.");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "main/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].main, "./specified", "When `main` attributes is set, the specified module is used as the main entry point. And the attribute is normalized");
+    },
+    checkManifest: function (options) {
+      test.assertEqual(options.main, name + "/specified",
+        "Manifest `main` is correct, i.e. absolute path to main module.");
+    }
+  });
+}
+
+exports.testManifestWithMainSameAsPackage = function (test) {
+  let name = "main-with-package-name";
+  runTest(test, {
+    packagesPath: "main/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].main, "./main-with-package-name", "When `main` attributes is set, the specified module is used as the main entry point. And the attribute is normalized");
+    },
+    checkManifest: function (options) {
+      test.assertEqual(options.main, name + "/" + name,
+        "Manifest `main` is correct, i.e. absolute path to main module.");
+    }
+  });
 }
 
 exports.testDataFolder = function (test) {
   let name = "main-package-data";
-  let packages = getPackages("data-folder/" + name);
-  test.assertEqual(packages[name].data.length, 1, "We got only one data folder registered");
-  test.assertEqual(packages[name].data[0], "data", "When nothing is specified in package.json about data folder, folder `data` is taken as default.");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "data-folder/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].data.length, 1, "We got only one data folder registered");
+      test.assertEqual(packages[name].data[0], "data", "When nothing is specified in package.json about data folder, folder `data` is taken as default.");
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["self"],
+                    "`main` requires `self`");
+    }
+  });
 }
 
 exports.testDataFolderInDependency = function (test) {
   let name = "main-package";
   let dependency = "dependency";
-  let packages = getPackages("data-folder/dependency-data");
-  test.assertEqual(packages[name].data.length, 0, "We don't have data folder in main package");
-  test.assertEqual(packages[dependency].data.length, 1, "We have one data folder in the dependency package");
-  test.assertEqual(packages[dependency].data[0], "data", "When nothing is specified in package.json about data folder, folder `data` is taken as default.");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "data-folder/dependency-data",
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].data.length, 0, "We don't have data folder in main package");
+      test.assertEqual(packages[dependency].data.length, 1, "We have one data folder in the dependency package");
+      test.assertEqual(packages[dependency].data[0], "data", "When nothing is specified in package.json about data folder, folder `data` is taken as default.");
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, dependency, "lib", "main.js", ["self"],
+                    "`main` requires `self`");
+    }
+  });
 }
 
 exports.testPackageModuleRequire = function (test) {
   let name = "main";
-  let packages = getPackages("require/package-module");
-  test.assertEqual(Object.keys(packages).sort().join(", "), 
-    ["dependency", name].join(", "), 
-    "We have our two packages");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "require/package-module",
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(Object.keys(packages).sort().join(", "), 
+                       ["dependency", name].join(", "), 
+                       "We have our two packages");
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["dependency/module"],
+                    "`main` requires another module's package");
+    }
+  });
 }
 
 exports.testPackageMainRequire = function (test) {
   let name = "main";
-  let packages = getPackages("require/package-main");
-  test.assertEqual(Object.keys(packages).sort().join(", "), 
-    ["dependency", name].join(", "), 
-    "We have our two packages");
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "require/package-main",
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(Object.keys(packages).sort().join(", "), 
+                       ["dependency", name].join(", "), 
+                       "We have our two packages");
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["dependency"],
+                    "`main` requires another module's main");
+    }
+  });
+}
+
+exports.testPackageMainRequireWithSameName = function (test) {
+  let name = "main";
+  runTest(test, {
+    packagesPath: "require/package-main-with-same-name",
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(Object.keys(packages).sort().join(", "), 
+                       ["dependency", name].join(", "), 
+                       "We have our two packages");
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["dependency"],
+                    "`main` requires another module's main");
+    }
+  });
 }
 
 exports.testRelativeRequire = function (test) {
   let name = "relative";
-  let packages = getPackages("require/" + name);
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "require/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["./same-directory"],
+                    "`main` requires a module in same directory");
+      assertRequire(test, options, name, "lib", "same-directory.js", ["./sub-folder/module"],
+                    "`same-directory` requires a module in a sub-folder");
+    }
+  });
 }
 
 exports.testMagicRelativeRequire = function (test) {
   let name = "magic-relative";
-  let packages = getPackages("require/" + name);
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "require/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      
+    },
+    checkManifest: function (options) {
+      assertRequire(test, options, name, "lib", "main.js", ["same-directory"],
+                    "`main` requires a module in same directory");
+      assertRequire(test, options, name, "lib", "same-directory.js", ["sub-folder/module"],
+                    "`same-directory` requires a module in a sub-folder");
+    }
+  });
 }
 
 exports.testJIDWithoutPackageId = function (test) {
   let name = "no-package-id";
-  let packages = getPackages("jid/" + name);
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "jid/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      
+    },
+    checkManifest: function (options) {
+      let jid = options.jetpackID;
+      test.assert(jid.match(/jid0-\d+@jetpack/), "Jetpack ID is a generated one");
+    }
+  });
 }
 
 exports.testJIDWithPackageId = function (test) {
   let name = "with-package-id";
-  let packages = getPackages("jid/" + name);
-  
-  packages = includeApiUtils(packages);
-  
-  let options = AddonOptions.buildForRun({
-      packages: packages,
-      mainPackageName : name,
-    });
-  
-  let xpiPath = getXpiPath();
-  
-  let newOptions = XpiBuilder.build(options, name, xpiPath);
-  runWithinAndCheck(test, xpiPath, newOptions.jetpackID);
+  runTest(test, {
+    packagesPath: "jid/" + name,
+    mainPackageName: name,
+    
+    checkPackages: function (packages) {
+      test.assertEqual(packages[name].id, "package-id", "We have a package id.");
+    },
+    checkManifest: function (options) {
+      let jid = options.jetpackID;
+      test.assertEqual(jid, "package-id@jetpack", "Jetpack ID is built on top of package id");
+    }
+  });
 }
